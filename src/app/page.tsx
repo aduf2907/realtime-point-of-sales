4aa084c8335f0697bf3c332b9abed2c91f3793fd
aduf <dufalmira@gmail.com>
2026-06-18 17:37:15 +0700

    darkmode config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { DarkmodeToggle } from "@/components/common/darkmode-toggle";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
@@ -6,7 +7,8 @@ export default function Home() {
   return (
     <div>
       <Input />
-      <Button>Hello</Button>
+      <Button className="dark: bg-amber-400">Hello</Button>
+      <DarkmodeToggle />
     </div>
   );
 }
